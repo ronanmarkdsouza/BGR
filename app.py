884a339c04fc000fc73f8fa1442e849c8e25b534
Ronan Mark D'souza <93206604+ronanmarkdsouza@users.noreply.github.com>
2022-11-06 22:38:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def index():
     return render_template('index.html', gender=gender)
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    app.run(host="0.0.0.0", port="5000")
